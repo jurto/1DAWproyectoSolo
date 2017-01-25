0b1991225854cc2c576793fc03c9602adf21a58d
PixelZer0 <pixelzero@gmail.com>
2017-01-25 13:38:23 +0100

    Versión 1.0 - Proyecto creado

---
+++ MundoHola/src/mundohola/MundoHola.java
@@ -0,0 +1,17 @@
+package mundohola;
+
+/**
+ *
+ * @author Team
+ */
+public class MundoHola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Versión 1.0");
+    }
+    
+}
